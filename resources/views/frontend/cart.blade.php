76ccd8cd03d784a9f46ddd911fc6a57ad336d16d
jucola98 <gabriele.colapelle@gmail.com>
2020-01-09 18:25:05 +0100

    poca roba

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -105,53 +105,27 @@
                                 </div>
                             </form>
                         </div>
-                        <div class="row">
-                            <div class="col-md col-sm-6">
-                                <div class="heading chk">
-                                    <h2 class="section-title"> calculate shipping  <span class="icon_minus-06"></span> </h2>                              
-                                </div>
-                                <hr class="divider">
-                                <div class="calculate-wrap ptb-20">
-                                    <form class="calculate-form">
-                                        <div class="form-group">
-                                            <div class="search-selectpicker selectpicker-wrapper">
-                                                <select
-                                                    class="selectpicker input-price" data-live-search="true" data-width="100%"
-                                                    data-toggle="tooltip" title="United Kingdom (UK)">
-                                                    <option>United Kingdom (UK)</option>
-                                                    <option>United State (US)</option>
-                                                    <option>United Arab Emirates (UAE)</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" placeholder="State / Country" class="form-control">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" placeholder="Adddress " class="form-control">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" placeholder="Postcode / Zip" class="form-control">
-                                        </div>
-                                        <div class="form-group">
-                                            <button class="theme-btn btn submit-btn" type="submit"> <b> get a quote </b> </button>
-                                        </div>
-                                        
-                                    </form>
-                                </div>
-                            </div>
-                            
-                            <div class="col-md-6 col-sm-6">
-                                <div class="chk-total text-right">
-                                    <ul class="font-2">
+                        <section class="breadcrumb-bg-2">                
+                            <div class="container">
+                                <div class="site-breadcumb">                        
+                                <ul class="font-2">
                                         <li class="title-2"> sub total    <span> $120.10 </span> </li>
+                                        <br>
+                                        <li class="title-2"> shipping    <span> $7,00 </span> </li>
+                                        <br>
+                                        <li class="title-2"> tax 22%    <span> $120.10 </span> </li>
                                         <li class="section-title pt-20"> <b> grand total  <span class="red-color"> $120.10 </span> </b> </li>
                                         <li class="ptb-10"> <hr class="divider"> </li>
                                         <li> <button class="theme-btn-1 btn submit-btn" type="submit"> <b> proceed to checkout </b> </button> </li>
                                     </ul>
-                                    <a href="#"> Checkout with Mutilple Adresses </a>
-                                </div>
+                                </div>  
                             </div>
+                        </section>
+                        
+                            
+                            
+                                
+                            
                         </div>
                         <!-- / Shopping Cart Ends -->
                     </div>
--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -21,95 +21,9 @@
                         <!-- Checkout Starts -->
                         <div class="panel-group chk-panel" id="accordion">
                             <div class="panel">
-                                <div class="chk-heading">                              
-                                    <a class="section-title" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"> checkout method</a>                              
-                                </div>
+                                
                                 <div id="collapseOne" class="panel-collapse collapse in">
-                                    <div class="chk-body block-inline">
-                                        <div class="col-md-6">
-                                            <form class="chk-form" >
-                                                <h2 class="title-2">check as a guest or login</h2>
-                                                <p>Register with us for future convenience:</p>
-                                                <div class="form-group block-inline">
-                                                    <label class="radio-inline title-2"> <input type="radio" name="chk" value=""> <span> checkout as guest </span>  </label>
-                                                                                               
-                                                </div>
-                                                <h2 class="title-2"> register and save time ! </h2>
-                                                <p>Register with us for future convenience:</p>
-                                                <ul>
-                                                    <li> <span class="arrow_carrot-right"></span> Fast and easy check out </li>
-                                                    <li> <span class="arrow_carrot-right"></span> Easy access to your order history and status </li>
-                                                </ul>
-                                                
-                                                    <button class="theme-btn btn submit-btn" type="submit"> <b> Continue </b> </button>
-                                                
-                                            </form>
-                                        </div>
-
-                                        <div class="col-md-6">
-                                            <form class="registed-form">
-                                                <h2 class="title-2"> already registed ? </h2>
-                                                <div class="row justify-content-center">
-                                        <div class="col-md-9">
-                                            <div class="card">
-                                                <div class="card-header">{{ __('Login') }}</div>
-
-                                                <div class="card-body">
-                                                    <form method="POST" action="{{ route('login') }}">
-                                                        @csrf
-
-                                                        <div class="form-group row">
-                                                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }} </label>
-
-                                                            <div class="col-md-9">
-                                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"  placeholder="E-mail" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                                                @error('email')
-                                                                    <span class="invalid-feedback" role="alert">
-                                                                        <strong>{{ $message }}</strong>
-                                                                    </span>
-                                                                @enderror
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="form-group row">
-                                                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                                            <div class="col-md-9">
-                                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password"name="password" required autocomplete="current-password">
-
-                                                                @error('password')
-                                                                    <span class="invalid-feedback" role="alert">
-                                                                        <strong>{{ $message }}</strong>
-                                                                    </span>
-                                                                @enderror
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="form-group row">
-                                                            <div class="col-md-6 offset-md-4">
-                                                            
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="form-group row mb-0">
-                                                            <div class="col-md-8 offset-md-4">
-                                                                <button type="submit" class="theme-btn-1 larg-btn btn submit-btn">
-                                                                    {{ __('Login') }}
-                                                                </button>
-
-                                                                @if (Route::has('password.request'))
-                                                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                                                        {{ __('Forgot Your Password?') }}
-                                                                    </a>
-                                                                @endif
-                                                            </div>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </div>
-                                     </div>                                    
+                                                             
                                 </div>
                             </div>
                             <div class="heading chk">
@@ -133,13 +47,16 @@
                                             <input type="text" placeholder="State / Country" class="form-control">
                                         </div>
                                         <div class="form-group">
-                                            <input type="text" placeholder="Adddress " class="form-control">
+                                            <input type="text" placeholder="Address " class="form-control">
                                         </div>
                                         <div class="form-group">
                                             <input type="text" placeholder="Postcode / Zip" class="form-control">
                                         </div>
                                         <div class="form-group">
                                             <input type="text" placeholder="Phone number" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" placeholder="E-mail address" class="form-control">
                                         </div>
                                         
                                         
@@ -148,15 +65,108 @@
                             </div>
                             <div class="heading chk">
                                     <h2 class="section-title"> shipping method <span class="icon_minus-06"></span> </h2>                              
-                                </div>
-                                <div class="heading chk">
-                                    <h2 class="section-title"> payment  <span class="icon_minus-06"></span> </h2>                              
-                                </div>
                             </div>
-                            
-                            
+                            <hr class="divider">
+                            <p> DHL Express delivery: £4.50
+                                Next day delivery is offered for express delivery orders placed before 2 pm (Mon-Fri, excluding public holidays)
+                                The delivery date is included at checkout and in the shipping confirmation email
+                                Select 'Express delivery' on the final order confirmation page after you've selected the home delivery option
+                                Please note: express orders are not delivered on weekends 
+                                Once your order is shipped, we'll send a confirmation email with the expected delivery date. This date is also displayed at the checkout.
+                                 All orders from Zalando.co.uk can be delivered to an address located in the UK only. We do not offer international shipping.
+                            </p>
+                            <div class="heading chk">
+                                    <h2 class="section-title"> payment  <span class="icon_minus-06"></span> </h2>                              
+                            </div>
+                            <hr class="divider">
+                            <div class="container">
+                                <div class="row">
+                                    <!-- You can make it whatever width you want. I'm making it full width
+                                    on <= small devices and 4/12 page width on >= medium devices -->
+                                    <div class="col-xs-12 col-md-4">
 
-                </section>
 
-            </article>
+                                    <!-- CREDIT CARD FORM STARTS HERE -->
+                                        <div class="panel panel-default credit-card-box">
+                                            <div class="panel-heading display-table" >
+                                                <div class="row display-tr" >
+                                                <h2 class="title-3">  payment information</h2>
+                                                    <div class="display" >                            
+                                                        <img class="img-responsive" src="http://i76.imgup.net/accepted_c22e0.png">
+                                                    </div>
+                                                </div>                    
+                                            </div>
+                                            <div class="panel-body">
+                                            <form role="form" id="payment-form">
+                                                <div class="row">
+                                                    <div class="col-xs-12">
+                                                        <div class="form-group">
+                                                            <h2 class="title-2">card number</h2>
+                                                                <div class="input-group">
+                                                                    <input 
+                                                                        type="tel"
+                                                                        class="form-control"
+                                                                        name="cardNumber"
+                                                                        placeholder="Valid Card Number"
+                                                                        autocomplete="cc-number"
+                                                                        required autofocus 
+                                                                    />
+                                                                    <span class="input-group-addon"></span>
+                                                            </div>
+                                                        </div>                            
+                                                    </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-xs-7 col-md-7">
+                                                            <div class="form-group">
+                                                            <h2 class="title-2">expiration date</h2>
+                                                                    <input 
+                                                                        type="tel" 
+                                                                        class="form-control" 
+                                                                        name="cardExpiry"
+                                                                        placeholder="MM / YY"
+                                                                        autocomplete="cc-exp"
+                                                                        required 
+                                                                    />
+                                                            </div>
+                                                        </div>
+                                                    <div class="col-xs-5 col-md-5 pull-right">
+                                                        <div class="form-group">
+                                                        <h2 class="title-2">cv code</h2>
+                                                                <input 
+                                                                    type="tel" 
+                                                                    class="form-control"
+                                                                    name="cardCVC"
+                                                                    placeholder="CVC"
+                                                                    autocomplete="cc-csc"
+                                                                    required
+                                                                />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                    
+                                                <div class="row">
+                                                    <div class="col-xs-12">
+                                                        <button class="theme-btn-1 larg-btn btn submit-btn" type="submit">Start Subscription</button>
+                                                    </div>
+                                                </div>
+                                                <div class="row" style="display:none;">
+                                                    <div class="col-xs-12">
+                                                        <p class="payment-errors"></p>
+                                                </div>
+                                            </div>
+                                    </form>
+                                    </div>
+                                    </div>            
+                                    <!-- CREDIT CARD FORM ENDS HERE -->
+
+
+                                </div>            
+                            </div>
+                        </div>
+                    </body>
+  
+                </div>
+            </section>
+        </article>
             @endsection
