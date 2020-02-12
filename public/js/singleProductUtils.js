01a785d6bb96ab2094c5354f9634582cfab26d8b
vittorio <vittorio.dantico@student.univaq.it>
2020-02-12 12:25:41 +0100

    FUNZIONA IL SINGLEPRODUCTgit add .!

--- public/js/singleProductUtils.js
+++ public/js/singleProductUtils.js
@@ -8,10 +8,26 @@ function changeAmounts(arrayTotal,arrayCart){
         $("#stock").append(arrayTotal[value-1].amount);
         $("#inCart").html("");
         var propval=$("#selsizes").prop("selectedIndex");
-        $("#inCart").append(arrayCart[propval]?arrayCart[propval].amount:"");
-        //console.log(arrayCart[$("#selsizes").prop("selectedIndex")].amount);
-        //$("#inCart").append(arrayTotal[value-1].amount);
-        //console.log($("#selsizes :selected").val());
+        $("#inCart").append(arrayCart[propval]?arrayCart[propval].amount:"no");
+        $("#availwrapper").html("");
+        if(arrayTotal[value-1].amount){
+            $("#visibilityCheck").show();
+            $("#availwrapper").append( function(){
+                return"<b>Available:</b> <span class=\"green-color\"> In Stock </span> ";
+            });
+            $("#amountselector").html("");
+            $("#amountselector").append("<a class=\"btn minus\" onclick='productTextHandler(false,"+arrayTotal[value-1].amount+");'><i class=\"icon_minus-06\"></i></a><input type=\"number\" title=\"Qty\" value=\"1\" name=\"quantity\" min=\"1\" max=\""+arrayTotal[value-1].amount+"\" step=\"1\" class=\"form-control qty\" text=\"1\" id=\"quant\" onchange='changeValueInput(this,"+arrayTotal[value-1].amount+");'><a class=\"btn plus\" onclick='productTextHandler(true,"+arrayTotal[value-1].amount+");'><i class=\"icon_plus\"></i></a>");
+        }else{
+            $("#visibilityCheck").hide();
+            return"<b>Available:</b> <span class=\"red-color\"> Not In Stock </span> ";
         }
+        /*
+        <a class=\"btn minus\" onclick='productTextHandler(false,{{$details->first()->amount}});'><i class=\"icon_minus-06\"></i></a>
+        <input type=\"number\" title=\"Qty\" value=\"1\" name=\"quantity\" min=\"1\" max=\"{{$singart->stock}}\" step=\"1\" class=\"form-control qty\" text=\"1\" id=\"quant\" onchange='changeValueInput(this,{{$singart->stock}});'>
+        <a class=\"btn plus\" onclick='productTextHandler(true,{{$details->first()->amount}});'><i class=\"icon_plus\"></i></a>  */     }
     );
+}
+function checkIfCartExists(cart,details){
+    return cart!=undefined?details-cart:details;
+
 }
--- resources/views/frontend/singleProduct.blade.php
+++ resources/views/frontend/singleProduct.blade.php
@@ -93,21 +93,30 @@
                     <div class="product-availability">
                         <ul class="stock-detail list-items black-color">  
                             @if($singart->stock>0)                                    
-                                <li class=""> <i class="icon-layers icons"></i> <span> Only <b><span id="stock">{{$singart->stock}}</span></b> Left </span> <i class="arrow_carrot-down"></i> </li>
+                                <li class=""> <i class="icon-layers icons"></i> <span> Only <b><span id="stock">{{$details->first()->amount}}</span></b> Left </span> <i class="arrow_carrot-down"></i> </li>
                                 
                                 
                                     <li class=""> <i class="icon-handbag icons"></i> <span> You have <b><span id="inCart">
                                         
-                                        @if(!$cartamount->get()->isEmpty()))
+                                        @if(!$cartamount->get()->isEmpty())
                                             {{$cartamount->get()[0]->amount}}
+                                        @else
+                                            no
                                         @endif
                                     </span></b> products in your cart </span> <i class="arrow_carrot-down"></i> </li>
                                 
                                 <li>
-                                <b>Available:</b> <span class="green-color"> In Stock </span>  
-                            @else
-                                <b>Available:</b> <span class="red-color"> Not In Stock </span>  
+                               
+                            
+                                
                             @endif
+                            <div id="availwrapper"> 
+                                @if($details->first()->amount>0)
+                                <b>Available:</b> <span class="green-color"> In Stock </span> 
+                                @else
+                                <b>Available:</b> <span class="red-color"> Not In Stock </span>
+                                @endif
+                            </div>
                             </li>
                         </ul>                                                                             
                     </div>
@@ -121,53 +130,46 @@
                     <hr class="divider-2">   
                     <form action="{{route("testroute",["prodid"=>$singart->id])}}" method="post">
                     @csrf
-                    
-                    <div class="prod-btns">
-                        @if($singart->stock>0)
-                            @isset($cartamount->amount)
-                                @if($singart->stock-$cartamount->amount>0)
-                                <div class="quantity">
-                                    <a class="btn minus" onclick='productTextHandler(false,{{$singart->stock-$cartamount->amount}});'><i class="icon_minus-06"></i></a>
-                                    <input type="number" title="Qty" value="1" name="quantity" min="1" max="{{$singart->stock}}"step="1" class="form-control qty" text="1" id="quant" onchange='changeValueInput(this,{{$singart->stock}});'>
-                                    <a class="btn plus" onclick='productTextHandler(true,{{$singart->stock-$cartamount->amount}});'><i class="icon_plus"></i></a>
+                    <ul class="color-swatch-item">
+                        <select class="selectpicker"  id="selsizes" data-width="25%" name="sizeselect" onchange="changeAmounts({{$details}},{{$cartamount->get()}});"  >
+                            @foreach ( $details as $sizes)                                                   
+                                <option value="{{$sizes->id}}">{{$sizes->size}}</option>
+                            @endforeach
+                        </select>
+                    </ul>
+                    <div class="prod-btns" id="visibilityCheck"  @if($details->first()->amount<=0) style="display:none;"@endif>
+                        
+                         {{-- @if($details->first()->amount>0) --}}
+                         {{--   @isset($cartamount->amount) --}}
+                         {{--       @if($singart->stock-$cartamount->amount>0) --}}
+                                <div class="quantity" id="amountselector">
+                                    
+                                    <a class="btn minus" onclick='productTextHandler(false,{{$details->first()->amount-($cartamount->first()!=null?$cartamount->first()->amount:0)}});'><i class="icon_minus-06"></i></a>
+                                    <input type="number" title="Qty" value="0" name="quantity" min="1" max="{{$details->first()->amount-($cartamount->first()!=null?$cartamount->first()->amount:0)}}"step="1" class="form-control qty" text="1" id="quant" onchange='changeValueInput(this,{{$details->first()->amount-($cartamount->first()!=null?$cartamount->first()->amount:0)}});'>
+                                    <a class="btn plus" onclick='productTextHandler(true,{{$details->first()->amount-($cartamount->first()!=null?$cartamount->first()->amount:0)}});'><i class="icon_plus"></i></a>
                                 </div>
                                 <form action="{{route('wishlistadd')}}" method="post" class="formset">
-                                                        @csrf
-                                                        <button type="submit" value="{{$singart->id}}" name="prodid" id="heart"></button>
-                                                       
-                                                    </form>
-                                <div class="add-to-cart">
-                                 
+                                    @csrf
+                                    <button type="submit" value="{{$singart->id}}" name="prodid" id="heart"></button>                   
+                                </form>
+                                <div class="add-to-cart"> 
                                     <input class="theme-btn-1 btn cart-btn" type="submit" value="Add to Cart" />
-                                    
                                 </div>
-                               
-                                
-                                @endif
-                                
-                            @endisset   
-                            <ul class="color-swatch-item">
-                                
-                                        <select class="selectpicker"  id="selsizes" data-width="25%" name="sizeselect" onchange="changeAmounts({{$details}},{{$cartamount->get()}});"  >
-                                            @foreach ( $details as $sizes)                                                   
-                                                <option value="{{$sizes->id}}">{{$sizes->size}}</option>
-                                            @endforeach
-                                        </select>
-                            </ul>
-                            @empty($cartamount->amount)
-                                <div class="quantity">
-                                    <a class="btn minus" onclick='productTextHandler(false,{{$singart->stock}});'><i class="icon_minus-06"></i></a>
-                                    <input type="number" title="Qty" value="1" name="quantity" min="1" max="{{$singart->stock}}"step="1" class="form-control qty" text="1" id="quant" onchange='changeValueInput(this,{{$singart->stock}});'>
-                                    <a class="btn plus" onclick='productTextHandler(true,{{$singart->stock}});'><i class="icon_plus"></i></a>
-                                </div>
-                                <div class="add-to-cart">
-                                <input class="theme-btn-1 btn cart-btn" type="submit" value="Add to Cart" />
-                                </div>
-                            @endempty
-                        
-                        @else
-
-                        @endif
+                             {{--   @endif--}}
+                            {{--@endisset   --}}
+                           
+                                {{--@empty($cartamount->amount)--}}
+                                    <!--<div class="quantity">
+                                        <a class="btn minus" onclick='productTextHandler(false,{{$singart->stock}});'><i class="icon_minus-06"></i></a>
+                                        <input type="number" title="Qty" value="1" name="quantity" min="1" max="{{$singart->stock}}"step="1" class="form-control qty" text="1" id="quant" onchange='changeValueInput(this,{{$singart->stock}});'>
+                                        <a class="btn plus" onclick='productTextHandler(true,{{$singart->stock}});'><i class="icon_plus"></i></a>
+                                    </div>
+                                    <div class="add-to-cart">   
+                                        <input class="theme-btn-1 btn cart-btn" type="submit" value="Add to Cart" />
+                                    </div>
+                                {{-- @endempty --}}
+                            {{-- @else --}}
+                        {{--@endif --}}
                                                      
                     </div>
                     </form> 
